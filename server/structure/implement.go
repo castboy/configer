8c7766b4e1ffb5b38e0932f162e2e2ca60f19463
castboy <482819895@qq.com>
2020-04-14 17:30:18 +0800

    structure/implement.go

---
+++ server/structure/implement.go
@@ -0,0 +1,76 @@
+package structure
+
+import (
+	indexID2 "configer/server/structure/indexID"
+	"configer/server/structure/indexNameID"
+)
+
+type MarketDST struct {
+	MarketOwnerType indexNameID.MarketType `json:"market_owner_type" xorm:"market_type"`
+	DST             indexID2.DSTType       `xorm:"dst_type"`
+}
+
+func (md *MarketDST) FormatCheck() error {
+	return nil
+}
+func (md *MarketDST) IndexCheck() error {
+	return nil
+}
+
+func (md *MarketDST) AutoCondition() (cond string) {
+	return
+}
+
+// holiday.calc
+type HolidayCalc struct {
+	ID int
+	DateSymbol
+	TimeSpans []*TimeSpan
+}
+
+type DateSymbol struct {
+	Date,
+	Symbol string
+}
+
+type TimeSpan struct {
+	From,
+	To string
+}
+
+func (hc *HolidayCalc) FormatCheck() error {
+	return nil
+}
+
+func (hc *HolidayCalc) IndexCheck() error {
+	return nil
+}
+
+func (hc *HolidayCalc) AutoCondition() (cond string) {
+	return
+}
+
+// full.symbol.name
+type FullSymbolName struct {
+	Sl       SymbolLeverage
+	FullName string
+}
+
+type SymbolLeverage struct {
+	Symbol   string
+	Leverage int32
+}
+
+func (fs *FullSymbolName) FormatCheck() error {
+	return nil
+}
+
+func (fs *FullSymbolName) IndexCheck() error {
+	return nil
+}
+
+func (fs *FullSymbolName) AutoCondition() (cond string) {
+	return
+}
+
+
